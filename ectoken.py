11b0608f2cab4f9c804d5a2e67edfc4270448b71
Wolfgang Schnerring <ws@gocept.com>
2012-11-27 11:18:27 +0100

    Add check for maximum length (taken from the original Edgecast ec_encrypt.c example)

--- ectoken.py
+++ ectoken.py
@@ -5,6 +5,9 @@ bf = CDLL(pkg_resources.resource_filename(__name__, '_ecblowfish.so'))
 
 
 def ectoken_generate(key, string):
+    if len(string) > 512:
+        raise ValueError(
+            '%r exceeds maximum length of 512 characters' % string)
     if isinstance(string, unicode):
         string = string.encode('utf-8')
     string = 'ec_secure=%03d&%s' % (len(string) + 14, string)
