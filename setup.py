c1112f9aaeab9beb4395045e6c20185a33f05483
Wolfgang Schnerring <ws@gocept.com>
2012-11-27 11:06:12 +0100

    Indent according to PEP8

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-name, version = 'ectoken', '0.2'
+name, version='ectoken', '0.2'
 
 from distutils.core import setup, Extension
-from distutils.sysconfig import get_python_lib
 
-blowfish = Extension(
-    name = '_ecblowfish',
-    sources = ['blowfish.c'],
+blowfish=Extension(
+    name='_ecblowfish',
+    sources=['blowfish.c'],
     libraries=['crypto'],
 )
 
 setup(
-  name=name,
-  version=version,
-  description='Python implementation of EdgeCast Token (ectoken_generate)',
-  author="Sebastien Estienne",
-  author_email="sebastien.estienned@gmail.com",
-  url="https://github.com/sebest/ectoken-py",
-  py_modules=['ectoken'],
-  ext_modules = [blowfish],
+    name=name,
+    version=version,
+    description='Python implementation of EdgeCast Token (ectoken_generate)',
+    author="Sebastien Estienne",
+    author_email="sebastien.estienned@gmail.com",
+    url="https://github.com/sebest/ectoken-py",
+    py_modules=['ectoken'],
+    ext_modules=[blowfish],
 )
